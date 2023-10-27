a61a1577abe5a0e1dac5cafd8e363ffb4b932630
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2023-10-27 15:52:53 +0100

    activated seagrass

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,7 @@ else
   CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
 fi
 
-cmake -B build -S "${SRC_DIR}/gotm" -DFABM_BASE="${SRC_DIR}/fabm" -DGOTM_USE_STIM=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${PREFIX}" ${CMAKE_PLATFORM_FLAGS[@]} ${CMAKE_ARGS}
+cmake -B build -S "${SRC_DIR}/gotm" -DFABM_BASE="${SRC_DIR}/fabm" -DGOTM_USE_STIM=ON -DGOTM_USE_SEAGRASS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${PREFIX}" ${CMAKE_PLATFORM_FLAGS[@]} ${CMAKE_ARGS}
 cmake --build build --config Release --parallel ${CPU_COUNT}
 
 mkdir -p "${PREFIX}/bin"
